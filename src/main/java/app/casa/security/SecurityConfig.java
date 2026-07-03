1b876630b01b76c533a1774cf5168718c165e601
Nicola <nsprogramwork@mail.com>
2026-07-03 23:52:01 +0200

    fix cors

--- src/main/java/app/casa/security/SecurityConfig.java
+++ src/main/java/app/casa/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package app.casa.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -24,6 +25,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/utente/login", "/utente/registrazione").permitAll()
                         .anyRequest().authenticated()
                 )
